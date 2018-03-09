e88c3c34e4545768932a080ce8758ba669d0920e
Marie-Lynne Block <marielynne.block@gmail.com>
2018-03-09 20:29:00 +0100

    Primitive datatypes exercises

---
+++ Section 2/CharBoolean.java
@@ -0,0 +1,12 @@
+public class CharBoolean {
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println("Unicode output was: " + myChar);
+
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+    }
+
+}
