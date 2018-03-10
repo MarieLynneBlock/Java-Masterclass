58a4cd670bb6a8c51f81fdbe6a2514c82b6cb1e2
Marie-Lynne Block <marielynne.block@gmail.com>
2018-03-10 11:38:59 +0100

    Refactor project structure

--- Section 2/Char.java
+++ Section 2/Char.java
@@ -1,12 +1,14 @@
 public class Char {
 
     public static void main(String[] args) {
+
         // 1. Find the code for the registered symbol
         // 2. Create a variable of the type char and assign it the unicode value for that symbol
         // 3. Display on screen
 
         char unicodeSymbol = '\u00AE';
         System.out.println("The symbol: " + unicodeSymbol);
+
     }
 
 }
--- Section 2/CharBoolean.java
+++ Section 2/CharBoolean.java
@@ -1,6 +1,7 @@
 public class CharBoolean {
 
     public static void main(String[] args) {
+
         char myChar = '\u00A9';
         System.out.println("Unicode output was: " + myChar);
 
